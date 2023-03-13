097d96335936ca64ef3200fd7390a94095f0fb7f
Piotr Syga <piotr.syga@int.pl>
2023-03-13 18:07:31 +0100

    Zadanie domowe z dnia 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var age = 33;
+var sex = "Kobieta";
+
+if (name == "Ewa" && age == 33) ;
+{
+    Console.WriteLine("Ewa,lat 33");
+}
