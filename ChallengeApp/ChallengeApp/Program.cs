d2cc09e11f156260766b5a419ea3be789a8badaa
Piotr Syga <piotr.syga@int.pl>
2023-03-13 19:01:47 +0100

    poprawka zadania 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,8 +1,37 @@
-﻿var name = "Ewa";
+﻿// Podaj imię
+var name = "Ewa";
+//Podaj wiek
 var age = 33;
+//Podaj płeć
 var sex = "Kobieta";
+
+// zweryfikuj dane
+// 1. "Kobieta poniżej 30 lat"
+
+if (sex == "Kobieta" && age < 30) ;
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+};
+
+
+// 2. Ewa, lat 33
 
 if (name == "Ewa" && age == 33) ;
 {
     Console.WriteLine("Ewa,lat 33");
+};
+
+
+// 3. Niepełnoletni mężczyzna
+var Myage = 17;
+var Mysex = "Mężczyzna";
+
+if (Myage == 18 && Mysex == "Mężczyzna") 
+{
+    Console.WriteLine("pełnoletni mężczyzna");
 }
+else if (Myage < 18 && Mysex == "Mężczyzna") 
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
+
